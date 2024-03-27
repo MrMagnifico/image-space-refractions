3a8d1f5fbee6a99b0e406f94b03378168f37632b
William Narchi <willy.narchi@gmail.com>
2024-03-27 23:14:13 +0100

    Render normals to texture

--- src/main.cpp
+++ src/main.cpp
@@ -28,7 +28,8 @@ int main(int argc, char* argv[]) {
     GPUMesh& mainMesh               = subMeshes[0];
     const Shader debugShader        = ShaderBuilder().addStage(GL_VERTEX_SHADER, utils::SHADERS_PATH / "deferred.vert")
                                                      .addStage(GL_FRAGMENT_SHADER, utils::SHADERS_PATH / "debug.frag").build();
-    const Shader depthTexWrite      = ShaderBuilder().addStage(GL_VERTEX_SHADER, utils::SHADERS_PATH / "deferred.vert").build();
+    const Shader geomInfoWrite      = ShaderBuilder().addStage(GL_VERTEX_SHADER, utils::SHADERS_PATH / "deferred.vert")
+                                                     .addStage(GL_FRAGMENT_SHADER, utils::SHADERS_PATH / "write-normal.frag").build();
     const Shader screenQuad         = ShaderBuilder().addStage(GL_VERTEX_SHADER, utils::SHADERS_PATH / "screen-quad.vert")
                                                      .addStage(GL_FRAGMENT_SHADER, utils::SHADERS_PATH / "screen-quad.frag").build();
 
@@ -38,18 +39,27 @@ int main(int argc, char* argv[]) {
             return;
     });
 
-    // Create depth texture and accompanying framebuffer
+    // Create depth texture
     std::array<GLint, 4> swizzleAllAccessRed = {GL_RED, GL_RED, GL_RED, GL_RED};
-    GLuint texDepth, depthTexFramebuffer;
+    GLuint texDepth;
     glCreateTextures(GL_TEXTURE_2D, 1, &texDepth);
     glTextureStorage2D(texDepth, 1, GL_DEPTH_COMPONENT32F, window.getWindowSize().x, window.getWindowSize().y);
-    glTextureParameteri(texDepth, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-    glTextureParameteri(texDepth, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-    glTextureParameteri(texDepth, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTextureParameteri(texDepth, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTextureParameteri(texDepth, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTextureParameteri(texDepth, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
     glTextureParameteriv(texDepth, GL_TEXTURE_SWIZZLE_RGBA, swizzleAllAccessRed.data());
-    glCreateFramebuffers(1, &depthTexFramebuffer);
-    glNamedFramebufferTexture(depthTexFramebuffer, GL_DEPTH_ATTACHMENT, texDepth, 0);
+
+    // Create normal texture
+    GLuint texNormal;
+    glCreateTextures(GL_TEXTURE_2D, 1, &texNormal);
+    glTextureStorage2D(texNormal, 1, GL_RGB16F, window.getWindowSize().x, window.getWindowSize().y);
+    glTextureParameteri(texNormal, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTextureParameteri(texNormal, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+    // Attach depth and normal textures to framebuffer
+    GLuint geomInfoFramebuffer;
+    glCreateFramebuffers(1, &geomInfoFramebuffer);
+    glNamedFramebufferTexture(geomInfoFramebuffer, GL_DEPTH_ATTACHMENT, texDepth, 0);
+    glNamedFramebufferTexture(geomInfoFramebuffer, GL_COLOR_ATTACHMENT0, texNormal, 0);
 
     // Enable depth testing
     glEnable(GL_DEPTH_TEST);
@@ -67,23 +77,23 @@ int main(int argc, char* argv[]) {
         const glm::mat3 normalModel = glm::inverseTranspose(glm::mat3(model));
         const glm::mat4 mvp         = trackball.projectionMatrix() * trackball.viewMatrix() * model;
 
-        // Render to depth texture
-        glBindFramebuffer(GL_FRAMEBUFFER, depthTexFramebuffer);
-        glClear(GL_DEPTH_BUFFER_BIT);
+        // Render to geometry info textures
+        glBindFramebuffer(GL_FRAMEBUFFER, geomInfoFramebuffer);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         glViewport(0, 0, window.getWindowSize().x, window.getWindowSize().y);
-        depthTexWrite.bind();
+        geomInfoWrite.bind();
         glUniformMatrix4fv(0, 1, GL_FALSE, glm::value_ptr(mvp));
         glUniformMatrix4fv(1, 1, GL_FALSE, glm::value_ptr(model));
         glUniformMatrix3fv(2, 1, GL_FALSE, glm::value_ptr(normalModel));
-        mainMesh.draw(depthTexWrite);
+        mainMesh.draw(geomInfoWrite);
 
-        // Draw the depth texture to the screen
+        // Draw the normal texture to the screen
         glBindFramebuffer(GL_FRAMEBUFFER, 0);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         glViewport(0, 0, window.getWindowSize().x, window.getWindowSize().y);
         screenQuad.bind();
         glActiveTexture(GL_TEXTURE0);
-        glBindTexture(GL_TEXTURE_2D, texDepth);
+        glBindTexture(GL_TEXTURE_2D, texNormal);
         glUniform1i(0, 0);
         utils::renderQuad();
 
